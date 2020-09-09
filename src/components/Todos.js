11074ee07581dd28cc147d76295149960b2b26a9
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2020-09-09 10:25:08 +0100

    .

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import Todoitem from './Todoitem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component{
     render() {
-        return(
-            <div>
-            <h1>Todos</h1>
-            </div>
-        );
+        console.log(this.props.todos);
+        return this.props.todos.map((todo) => (
+        <Todoitem key={todo.id} todo={todo}/>
+        ));
     }
+
 }
+
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
 export default Todos;
