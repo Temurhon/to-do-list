844338b60cda294dbcf2d9eae4de06c65cff017c
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2020-09-09 11:09:14 +0100

    .

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class Todoitem extends Component {
     render() {
         return (
-            <div>
+            <div style={{backgroundColor: '#f4f4f4'}}>
                 <p>{this.props.todo.title}</p>
             </div>
         )
     }
+}
+
+
+Todoitem.propTypes = {
+    todos: PropTypes.object.isRequired
 }
 
 export default Todoitem
