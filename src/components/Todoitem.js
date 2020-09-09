4847ac30a3db666115f5de38c077d5f8d4a8dbb6
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2020-09-09 11:44:29 +0100

    addded styling

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class Todoitem extends Component {
+    getStyle = () => {
+        return{
+            background: '#f4f4f4',
+            testDecoration: this.props.todo.completed ? 'line-through' : 'none',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ?
+            'line-through' : 'none'
+        }
+    }
     render() {
         return (
-            <div style={{backgroundColor: '#f4f4f4'}}>
-                <p>{this.props.todo.title}</p>
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox"/> {''}
+                    {this.props.todo.title}</p>
             </div>
         )
     }
@@ -15,5 +27,7 @@ export class Todoitem extends Component {
 Todoitem.propTypes = {
     todos: PropTypes.object.isRequired
 }
+
+
 
 export default Todoitem
