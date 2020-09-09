325828f5e411da57ac93c4c2df6839199b49d19f
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2020-09-09 13:22:02 +0100

    added mark complete function

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,10 +3,10 @@ import Todoitem from './Todoitem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component{
+  
     render() {
-        console.log(this.props.todos);
         return this.props.todos.map((todo) => (
-        <Todoitem key={todo.id} todo={todo}/>
+        <Todoitem key={todo.id} todo={todo} markComplete={this.props.markComplete} />
         ));
     }
 
